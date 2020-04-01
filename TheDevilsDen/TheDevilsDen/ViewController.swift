9f6ea2d90c326c1ae630aa6f7404b95e5f77e847
Adam Rowe <adaminsley@gmail.com>
2020-04-01 19:28:04 -0400

    logo and sign up buttons

---
+++ TheDevilsDen/TheDevilsDen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheDevilsDen
+//
+//  Created by Adam Rowe on 4/1/20.
+//  Copyright © 2020 Adam Rowe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
